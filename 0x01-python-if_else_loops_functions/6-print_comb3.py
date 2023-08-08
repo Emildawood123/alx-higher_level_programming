a28cd2551455d25c67d25f79fc8917928d858563
emil dawood <edawood997@gmail.com>
2023-08-08 06:43:37 -0700

    9 comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 100):
     if i > 10 and i // 10 >= i % 10:
         continue
     if i == 89:
-        print(f"{i}")
+        print("{}".format(i))
     else:
         print("%02d, " % (i), end='')
