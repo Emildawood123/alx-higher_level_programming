987716d57130546181ccb35befa2a6a9bfc1de86
emil dawood <edawood997@gmail.com>
2023-08-09 22:54:16 -0700

    with pycodestyle

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+else:
+    a = sys.argv[1]
+    b = sys.argv[3]
+    if (sys.argv[2] == '+'):
+        print("{} + {} = {}".format(a, b, add(int(a), int(b))))
+        exit(0)
+    elif (sys.argv[2] == '-'):
+        print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
+        exit(0)
+    elif (sys.argv[2] == '*'):
+        print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
+        exit(0)
+    elif (sys.argv[2] == '/'):
+        print("{} / {} = {}".format(a, b, div(int(a), int(b))))
+        exit(0)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,8 +6,8 @@ if (len(sys.argv) == 1):
     print(0)
 else:
     for i in sys.argv:
-       if (count == 0):
-           count += 1
-           continue
-       res += int(sys.argv[count])
+        if (count == 0):
+            count += 1
+            continue
+        res += int(sys.argv[count])
     print("{}".format(res))
