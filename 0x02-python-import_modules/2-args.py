8f07b29edc522b3c9a67eafd1dce369c34767f24
emil dawood <edawood997@gmail.com>
2023-08-11 01:23:15 -0700

    last commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,11 @@ if __name__ == "__main__":
     count = 0
     if (len(sys.argv) == 1):
         print("0 arguments.")
+    elif len(sys.argv) == 2:
+        count += 1
+        print("{} argument:\n{}: {}".format(count, count, sys.argv[count]))
     else:
-        print("{} arguments:", len(sys.argv) - 1)
+        print("{} arguments:".format(len(sys.argv) - 1))
         for i in sys.argv:
             if (count == 0):
                 count += 1
