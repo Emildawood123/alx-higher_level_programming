64db807949037afa2f0cfc557d6bb0a40c287fc3
emil <e@gmail.com>
2023-08-28 12:33:34 +0300

    return anytime

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,8 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         while(count < x):
-            print("{:d}".format(my_list[count], end=''))
+            print("{}".format(my_list[count], end=""))
             count += 1
     except IndexError:
         return (count)
+    return (count)
