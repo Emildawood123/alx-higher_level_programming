1ddedefccef6e3ae8b29e3eec3d29532c049fc7c
emil <e@gmail.com>
2023-08-29 14:52:19 +0300

    instatnation var

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Square class """
+class Square:
+    """ Square """
+    """
+    __init
+    Args:
+    size: size of square
+    """
+    def __init__(self, size):
+        __size = size
