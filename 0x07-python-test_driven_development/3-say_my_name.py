9a90692dc157288b6cef67625a2924b0491dbb19
Emil dawood <edawood997@gmail.com>
2023-09-06 20:04:58 +0300

    d

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" moudle """
+
+
 def say_my_name(first_name, last_name=""):
+    """ d """
     if (type(first_name) != str):
         raise TypeError("first_name must be a string")
     elif (type(last_name) != str):
