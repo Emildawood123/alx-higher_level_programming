00f39b7bf6f700309813210b4bd32f0c4fd72ef6
Emil dawood <edawood997@gmail.com>
2023-09-11 19:14:08 +0300

    d

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""moudle"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """class"""
+    def __init__(self, size):
+        self.__size = size
+        super(Square, self).integer_validator("size", size)
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        return (self.__size ** 2)
