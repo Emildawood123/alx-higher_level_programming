ba2592f763a8f406a4e3d0e274e6cae4b5a1c856
Emil dawood <edawood997@gmail.com>
2023-09-13 02:48:37 +0300

    done

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (attrs is None):
+            return (self.__dict__)
+        else:
+            for i in self.__dict__.keys:
+                if (i not in attrs):
+                    self.__delattr__(i)
+                return (self.__dict__)
