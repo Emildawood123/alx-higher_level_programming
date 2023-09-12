d22a111fa57b7597552713bde8251e29762806ce
Emil dawood <edawood997@gmail.com>
2023-09-12 17:22:25 +0300

    save to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """class"""
+    with open(filename, "w") as file:
+        file.write(json.loads(my_obj))
