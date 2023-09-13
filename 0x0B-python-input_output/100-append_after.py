8d96da808214e3962e672c6ec2b5c175b2f04248
Emil dawood <edawood997@gmail.com>
2023-09-13 03:50:21 +0300

    100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "a") as file:
+        num = 0
+        my_list = file.readlines()
+        for i in my_list:
+            num = len(i) + 2
+            if (search_string in i):
+                file.seek(num)
+                file.write(new_string, end="")
