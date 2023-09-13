143ff9153bf812f0214ee612db27a385a2eb09e4
Emil dawood <edawood997@gmail.com>
2023-09-13 03:39:54 +0300

    done

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        my_list = list(self.__dict__.keys())
+        d = dict()
+        if (attrs is None):
+            return (self.__dict__)
+        for i in my_list:
+            if (i in attrs):
+                d[i] = self.__dict__[i]
+        return (d)
+
+    def reload_from_json(self, json):
+        for key in json:
+            self.__dict__[key] = json[key]
