2e219939e90028495e7da351faaad56125cf0713
Emil dawood <edawood997@gmail.com>
2023-09-18 20:23:21 +0300

    d

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""class rectangle moudle"""
+"""models/rectangle.py"""
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """rectangle class"""
+    """Class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
