a864341b2306716846d795da4bff1b27b20465c8
Emil dawood <edawood997@gmail.com>
2023-09-18 18:37:38 +0300

    done

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test-base.py
@@ -0,0 +1,17 @@
+import unittest
+from models.base import Base
+"""Test=Base"""
+
+
+class test_Base(unittest.TestCase):
+    """unittest"""
+
+    def test_init(self):
+        a = Base()
+        self.assertEqual(a.id, 1)
+
+        a1 = Base()
+        self.assertEqual(a1.id, 2)
+
+        a2 = Base(10)
+        self.assertEqual(a2.id, 10)
