c3095a8802bc9fa42e28f2bda5405c79feb87e71
Emil dawood <edawood997@gmail.com>
2023-09-18 20:36:44 +0300

    :::::::

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""moudle"""
+"""class square moudle"""
 
 from models.rectangle import Rectangle
 
-"""class"""
-
 
 class Square(Rectangle):
-
+    """square class"""
     def __init__(self, size, x=0, y=0, id=None):
+        """size."""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """size."""
         return ("[{}] ({}) {}/{} - {}".format(__class__.__name__,
                 self.id, self.x, self.y, self.size))
 
     @property
     def size(self):
+        """size."""
         return self.width
 
     @size.setter
     def size(self, value):
+        """size."""
         self.width = value
         self.height = value
 
