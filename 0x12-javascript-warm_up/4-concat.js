7b020d8e81240f4982791545542d703286d88b61
Emil dawood <edawood997@gmail.com>
2023-11-13 16:12:58 +0200

    d

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const first = process.argv[2]
-const second = process.argv[3]
-const concat = first + ' is ' + second
-console.log(concat)
+const first = process.argv[2];
+const second = process.argv[3];
+const concat = first + ' is ' + second;
+console.log(concat);
