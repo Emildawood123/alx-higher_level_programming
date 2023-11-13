3394c11fa34668cf0c8cba5afe3d1092ee40439a
Emil dawood <edawood997@gmail.com>
2023-11-13 18:48:10 +0200

    d

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const len = porcess.argv.length;
 if (len === 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (len === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
