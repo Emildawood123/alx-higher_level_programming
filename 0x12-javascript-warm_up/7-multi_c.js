6694ac3cb39d18758926be750b4f61d48107f49d
Emil dawood <edawood997@gmail.com>
2023-11-13 16:51:50 +0200

    d

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const x = process.argv[2];
+if (x === undefined) {
+  console.log('Missing number of occurrences');
+} else if (parseInt(x) < 0) {
+  return;
+} else {
+    for (let i = 0;i < x; i++) {
+      console.log('C is fun');
+    }
+}
