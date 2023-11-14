912cd5002bbb5e064b995af0a70a38f075a7a18b
Emil dawood <edawood997@gmail.com>
2023-11-14 14:21:53 +0200

    d

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 || h > 0 || typeof h === 'number' || typeof w === 'number') {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
