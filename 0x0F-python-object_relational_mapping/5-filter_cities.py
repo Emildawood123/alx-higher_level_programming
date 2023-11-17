ee0e9745f9bd09f75ca4ca8333f0c86fdade1a1c
Emil dawood <edawood997@gmail.com>
2023-11-17 15:12:27 +0200

    5-filter_cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""import libraries"""
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    dataBase = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    myCursor = dataBase.cursor()
+    command = """SELECT cities.name
+    FROM cities INNER JOIN states ON
+    cities.state_id = states.id
+    WHERE BINARY states.name = '{}'
+    ORDER BY cities.id ASC""".format(sys.argv[4])
+    myCursor.execute(command)
+    result = myCursor.fetchall()
+    for i in result:
+        print(i)
+    myCursor.close()
+    dataBase.close()
