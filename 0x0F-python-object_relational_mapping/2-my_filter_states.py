381453c1f387d38ea6f1eda174e9abb630529fe1
Emil dawood <edawood997@gmail.com>
2023-11-17 14:51:06 +0200

    done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
-
+"""import libraries"""
+import sys
 import MySQLdb
-from sys import argv
-
-if __name__ == '__main__':
-    mydb = MySQLdb.connect(
+if __name__ == "__main__":
+    dataBase = MySQLdb.connect(
         host='localhost',
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         port=3306
     )
-    mycursor = mydb.cursor()
-
-    query = """SELECT * FROM states
-    WHERE name = '{}' ORDER BY ID ASC""".format(argv[4])
-
-    mycursor.execute(query)
-
-    result = mycursor.fetchall()
-    for row in result:
-        print(row)
-    mycursor.close()
-    mydb.close()
+    myCursor = dataBase.cursor()
+    command = """SELECT * FROM states
+    WHERE name = '{}' ORDER BY ID ASC""".format(sys.argv[4])
+    myCursor.execute(command)
+    result = myCursor.fetchall()
+    for i in result:
+        print(i)
+    myCursor.close()
+    dataBase.close()
