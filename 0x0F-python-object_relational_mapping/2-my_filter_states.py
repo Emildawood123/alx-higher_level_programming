3a171b966b330aa2c4b459b5aea4973091b69569
Emil dawood <edawood997@gmail.com>
2023-11-17 14:38:24 +0200

    d

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,9 +10,11 @@ if __name__ == "__main__":
         db=sys.argv[3],
         port=3306
     )
+    command = """SELECT * FROM states
+    WHERE BINARY name = '{}'
+    ORDER BY ID ASC""".format(sys.argv[4])
     myCursor = dataBase.cursor()
-    myCursor.execute("""SELECT * FROM states WHERE BINARY name = '{}'
-    ORDER BY ID ASC""".format(sys.argv[4]))
+    myCursor.execute()
     result = myCursor.fetchall()
     for i in result:
         print(i)
