5b88c6466f270298dd46db8a6e57784db5822681
Emil dawood <edawood997@gmail.com>
2023-11-17 15:01:25 +0200

    okey 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         db=sys.argv[3],
         port=3306
     )
-    command = """SELECT * FROM states WHERE BINARY name IS '{}'
+    command = """SELECT * FROM states WHERE BINARY name = '{}'
     ORDER BY ID ASC"""
     data = re.match("[A-Za-z\\s]+", sys.argv[4])
     myCursor = dataBase.cursor()
