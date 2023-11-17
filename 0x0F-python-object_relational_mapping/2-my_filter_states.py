96029647e3c41548bb2873663ce5a1288d9107f8
Emil dawood <edawood997@gmail.com>
2023-11-17 14:47:37 +0200

    d

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     mycursor = mydb.cursor()
 
     query = """SELECT * FROM states
-    WHERE BINARY name = '{}' ORDER BY ID ASC""".format(argv[4])
+    WHERE name = '{}' ORDER BY ID ASC""".format(argv[4])
 
     mycursor.execute(query)
 
