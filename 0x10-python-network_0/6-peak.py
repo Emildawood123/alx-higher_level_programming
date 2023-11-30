1518d5a9ac2029c65970613d9f72273fe4fdad92
Emil dawood <edawood997@gmail.com>
2023-11-30 11:46:58 +0200

    d

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,12 +4,5 @@
 
 def find_peak(list_of_integers):
     """function"""
-    diff = 0
-    if (len(list_of_integers) == 0):
-        return None
-    else:
-        for i in range(len(list_of_integers) - 1):
-            if (list_of_integers[i] - list_of_integers[i + 1] >= diff):
-                number = list_of_integers[i]
-                diff = list_of_integers[i] - list_of_integers[i + 1]
-        return number
+    num = sorted(list_of_integers)[-1]
+    return num
