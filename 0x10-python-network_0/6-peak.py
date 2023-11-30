45280793f27cc5290857497475200d4018531443
Emil dawood <edawood997@gmail.com>
2023-11-30 13:03:45 +0200

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """function"""
+from helperFile import merge_sort
 
 
 def find_peak(list_of_integers):
     """function"""
-    return sorted(list_of_integers)[-1]
+    length = len(list_of_integers)
+    list_of_integers = merge_sort(list_of_integers, 0, length - 1)
+    return list_of_integers[-1]
