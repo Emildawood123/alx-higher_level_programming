0564dc1adb3254116a465420c01cc2ae20fe5af2
Emil dawood <edawood997@gmail.com>
2023-11-30 13:09:34 +0200

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """function"""
-from helperFile import merge_sort
 
 
 def find_peak(list_of_integers):
@@ -8,3 +7,47 @@ def find_peak(list_of_integers):
     length = len(list_of_integers)
     list_of_integers = merge_sort(list_of_integers, 0, length - 1)
     return list_of_integers[-1]
+
+
+def merge_sort(array, start, end):
+    """Base case: if there is only one"""
+    if end <= start:
+        return
+    midpoint = (end + start) // 2
+    merge_sort(array, start, midpoint)
+    merge_sort(array, midpoint + 1, end)
+
+    merge(array, start, midpoint, end)
+
+
+def merge(array, start, midpoint, end):
+    left_length = midpoint - start + 1
+    right_length = end - midpoint
+
+    left_array = [0] * left_length
+    right_array = [0] * right_length
+
+    for i in range(left_length):
+        left_array[i] = array[start + i]
+
+    for j in range(right_length):
+        right_array[j] = array[midpoint + 1 + j]
+    i = j = 0
+    k = start
+    while i < left_length and j < right_length:
+        if left_array[i] <= right_array[j]:
+            array[k] = left_array[i]
+            i += 1
+        else:
+            array[k] = right_array[j]
+            j += 1
+        k += 1
+    while i < left_length:
+        array[k] = left_array[i]
+        i += 1
+        k += 1
+
+    while j < right_length:
+        array[k] = right_array[j]
+        j += 1
+        k += 1
