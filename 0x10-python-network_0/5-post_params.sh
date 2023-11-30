2efd1b5d2d716ae263d57bac8650c262a2eb1dbd
Emil dawood <edawood997@gmail.com>
2023-11-30 11:21:02 +0200

    done

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this for get the size of content body
-curl -s -H "email: test@gmail.com" -H "subject: I will always be here for PLD" -X POST "$1"
+curl -sL -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
