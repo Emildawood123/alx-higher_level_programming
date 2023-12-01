dc402e880569e29cd75525162c21701226fbb542
Emil dawood <edawood997@gmail.com>
2023-12-01 15:36:51 +0200

    done

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL..."""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    import json
+
+    headers = {'Accept': 'application/vnd.github+json',
+               'Authorization':  f'Bearer {sys.argv[2]}',
+               'X-GitHub-Api-Version': '2022-11-28'}
+    res = requests.get('https://api.github.com/user', headers=headers,)
+    try:
+        json_text = res.json()
+        if (json_text):
+            print('{}'.format(json_text.get("id")))
+    except json.JSONDecodeError:
+        print('None')
