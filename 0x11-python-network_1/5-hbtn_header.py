f1a81a8545072a0988a705541b5c4a7b8453ebcc
Emil dawood <edawood997@gmail.com>
2023-12-01 12:47:23 +0200

    task number five

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL..."""
+if __name__ == "__main__":
+    import requests
+    import sys
+    res = requests.get(sys.argv[1])
+    print(res.headers['X-Request-Id'])
