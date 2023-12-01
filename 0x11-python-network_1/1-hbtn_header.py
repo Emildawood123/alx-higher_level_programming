79a7209ac390de5ac46b70071d55f2420ad411df
Emil dawood <edawood997@gmail.com>
2023-12-01 12:17:26 +0200

    task number 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a URL,
- sends a request to the URL and displays the value
-of the X-Request-Id variable found in the header of the response."""
+"""Write a Python script that takes in a URL..."""
 if __name__ == "__main__":
     import urllib.request
     import sys
-    """import urllib"""
     with urllib.request.urlopen(sys.argv[1]) as url:
         print(url.headers["X-Request-id"])
