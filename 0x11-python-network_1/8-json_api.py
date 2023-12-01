bfb0d1d161ea8a780f46355c3c2301fcc010a347
Emil dawood <edawood997@gmail.com>
2023-12-01 13:56:44 +0200

    d

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,14 +5,15 @@
 if __name__ == "__main__":
     import requests
     import sys
-    dic = {"q": ""}
-    if (sys.argv == 2):
-        dic = {"q": sys.argv[1]}
-    resp = requests.post("http://0.0.0.0:5000/search_user", json=dic)
+    if (len(sys.argv) == 2):
+        dic = {'q': sys.argv[1]}
+    else:
+        dic = {'q': ''}
+    resp = requests.post("http://0.0.0.0:5000/search_user", data=dic)
     try:
         string = resp.json()
         if (string):
-            print("[{}}] {}".format(string.get("id"), string.get("name")))
+            print("[{}}] {}".format(string["id"], string["name"]))
         else:
             print("No result")
     except Exception:
