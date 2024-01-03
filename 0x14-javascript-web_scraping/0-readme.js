52eead467aba959a8091e287a8300fa3b28afcbb
Emil dawood <edawood997@gmail.com>
2024-01-03 15:06:11 +0200

    ddddddone

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 const process = require('process');
 fs.readFile(process.argv[2], 'utf-8', (err, res) => {
-  try {
-    console.log(res);
-  } catch {
+  if (err) {
     console.log(err);
+    return;
   }
+  console.log(res);
 });
